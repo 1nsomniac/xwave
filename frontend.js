ffa40f7b2635c64eb4c74bcb9a62139fc79b1b49
1nsomniac <samthakala@gmail.com>
2020-03-28 18:59:34 -0600

    websocket implemted and console logging

---
+++ frontend.js
@@ -0,0 +1,10 @@
+var ws = new WebSocket('ws://localhost:40510');
+
+ws.onopen = function () {
+    console.log('websocket is connected ...')
+    ws.send('connected')
+}
+
+ws.onmessage = function (ev) {
+    console.log(ev);
+}
